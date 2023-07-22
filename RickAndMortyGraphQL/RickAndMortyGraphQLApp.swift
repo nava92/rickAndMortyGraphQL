017b8c3899a938850b21c61d13d1d0ae50f4b5ce
Eduardo Navarrete Carmona <nava.0892@gmail.com>
2023-07-22 01:59:54 -0600

    Initial Commit

---
+++ RickAndMortyGraphQL/RickAndMortyGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyGraphQLApp.swift
+//  RickAndMortyGraphQL
+//
+//  Created by Eduardo Navarrete Carmona on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
